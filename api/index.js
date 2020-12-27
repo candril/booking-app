b09103c9f82843dd127c119fb0edf66c52ef30e3
Stefan Lüdin <stefan.luedin@cycl.ch>
2020-12-27 10:12:07 +0100

    add very basic form implementation

--- api/index.js
+++ api/index.js
@@ -2,6 +2,5 @@ module.exports = (req, res) => {
   res.json({
     body: req.body,
     query: req.query,
-    cookies: req.cookies,
   })
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,90 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
 import { parse } from "query-string";
 
 function App() {
-  useEffect(() => {
-    const search = parse<{ sheetUrl: string }>(window.location.search);
-    console.log(search);
-  }, []);
+  const { displayName, mail } = parse<{ displayName: string; mail: string }>(
+    window.location.search
+  );
 
   return (
     <div>
-      <BookingDetailsForm />
+      <BookingDetailsForm
+        displayName={displayName}
+        mail={mail}
+        onSave={handleSave}
+      />
     </div>
   );
+
+  async function handleSave(data: { description: string }) {
+    await fetch({
+      url: "/api",
+      method: "POST",
+      // @ts-ignore
+      body: JSON.stringify(data),
+      // @ts-ignore
+      headers: { "content-type": "application/json" },
+    });
+  }
 }
 
-function BookingDetailsForm() {
+function BookingDetailsForm({
+  displayName,
+  mail,
+  onSave,
+}: {
+  displayName: string;
+  mail: string;
+  onSave: (data: { description: string }) => void;
+}) {
+  const [description, setDescription] = useState("");
   return (
-    <form>
-      <input type="hidden" name="SQF_START" />
-      <input type="hidden" name="SQF_END" />
-    </form>
+    <>
+      <Form>
+        <input type="hidden" name="SQF_START" />
+        <input type="hidden" name="SQF_END" />
+
+        <label htmlFor="mail">E-Mailaddresse:</label>
+        <TextField type="email" name="mail" disabled={true} value={mail} />
+
+        <label htmlFor="displayName">Name:</label>
+        <TextField
+          type="text"
+          name="displayName"
+          disabled={true}
+          value={displayName}
+        />
+
+        <label htmlFor="description">Bemerkungen:</label>
+        <TextArea
+          value={description}
+          onChange={(e) => setDescription(e.currentTarget.value)}
+          rows={3}
+          name="description"
+        />
+        <button onClick={() => onSave({ description })}>
+          Reservierung abschicken
+        </button>
+      </Form>
+    </>
   );
 }
 
 export default App;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+
+  max-width: 900px;
+`;
+
+const TextField = styled.input`
+  flex: 1 0 auto;
+`;
+
+const TextArea = styled.textarea`
+  flex: 1 0 auto;
+  resize: none;
+`;
